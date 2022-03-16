1628389f6b5c74d09bad1fd758056f0ee06c4b36
Jhons <doa.j17@yahoo.com>
2022-03-16 17:49:59 -0600

    La manera que puso github no sirve

---
+++ app/src/main/java/com/joncabdev/noteapp/components/NoteComponents.kt
@@ -0,0 +1,11 @@
+package com.joncabdev.noteapp.components
+
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+
+@Composable
+fun NoteInputText(modifier: Modifier = Modifier){
+
+
+}
